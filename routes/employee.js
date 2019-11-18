729cadc7e7a315d6a61dffaa43ed33eda0a6b00b
55360105 <55360105+neilgupte75@users.noreply.github.com>
2019-11-18 17:13:19 -0500

    added employee main page routes

--- routes/employee.js
+++ routes/employee.js
@@ -11,5 +11,43 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/empprof', async (req, res) => {
+  try{
+  res.render('templates/employee_profile');
+  res.status(200);
+    }catch (e) {
+    res.status(500).json({error: e});
+  }
+});
+
+
+router.get('/employeehours', async (req, res) => {
+  try{
+  res.render('templates/employee_hours');
+  res.status(200);
+    }catch (e) {
+    res.status(500).json({error: e});
+  }
+});
+
+router.get('/employeereports', async (req, res) => {
+  try{
+  res.render('templates/employee_reports');
+  res.status(200);
+    }catch (e) {
+    res.status(500).json({error: e});
+  }
+});
+
+
+router.get('/employeeconman', async (req, res) => {
+  try{
+  res.render('templates/employee_con_man');
+  res.status(200);
+    }catch (e) {
+    res.status(500).json({error: e});
+  }
+});
+
 
 module.exports = router;
