4b39b609d0908090b1b6d21b9353a99d436c3335
parthxparab <parthxparab@host-studentw-163-60.dhcp.stevens-tech.edu>
2019-12-11 17:16:09 -0500

    added emp details

---
+++ views/templates/transaction.js
@@ -0,0 +1,6 @@
+document.getElementById("trans").onclick = function () {
+    document.getElementById("demo").innerHTML = "Hello World";
+console.log("reached here")
+};
+
+console.log("reached here")
